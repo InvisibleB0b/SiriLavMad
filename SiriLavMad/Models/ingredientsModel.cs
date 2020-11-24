7687212bc093e9678ed6cee6b406caa3c6806498
Stefan de Haas Hansen <stef9239@edu.easj.dk>
2020-11-24 13:45:24 +0100

    Shopping list in the works

---
+++ SiriLavMad/Models/ingredientsModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiriLavMad.Models
+{
+    public class ingredientsModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+       
+        public string Amount { get; set; }
+
+
+        public ingredientsModel(int id, string name, string amount)
+        {
+            Id = id;
+            Name = name;
+            Amount = amount;
+        }
+
+        public ingredientsModel()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Amount: {Amount}";
+        }
+    }
+}
