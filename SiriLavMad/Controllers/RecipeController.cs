8671f3d8ca51df5c14bdf0860761a034c4f8ebb2
Revilo321 <oliverwii@live.dk>
2020-12-02 21:32:40 +0100

    cleaned up

--- SiriLavMad/Controllers/RecipeController.cs
+++ SiriLavMad/Controllers/RecipeController.cs
@@ -1,12 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using SiriLavMad.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using SiriLavMad.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -151,9 +149,20 @@ namespace SiriLavMad.Controllers
         }
         
         //POST: /recipe/
-        public void Post([FromBody] Recipe value)
+        public void PostRecipe([FromBody] Recipe obj)
         {
-            new ManageRecipes().Create(value);
+            string queryString = $"INSERT INTO Recipes (Id,Title,Last_made) VALUES ({obj.Id},'{obj.Title}','{DateTime.Now}')";
+            
+            string connectionString =
+                "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SiriDB; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+                command.Connection.Close();
+            }
+
         }
 
 
