d760b22f6edd937c9f2d1df854dd9bb024bd61d8
Andreasb95 <58553766+Andreasb95@users.noreply.github.com>
2020-12-06 12:34:35 +0100

    Forslag til løsning til pi
    Mangler: pyhton console program kan blive ved med at køre og få nye inputs istedet for kun at kunne tage 1.
    
    UI: Der mangler at forbinde website med api. en ide kunne være at der kommer en dropbox og når man klikker på den viser den en liste over de ord man har inputtet i pyhton applicationen

---
+++ PythonApplication/PythonApplication/PythonApplication.py
@@ -0,0 +1,33 @@
+import socket
+
+
+
+while True:
+
+		print("Insert Search word")
+
+		msgFromClient       = input()
+
+		bytesToSend         = str.encode(msgFromClient)
+
+		serverAddressPort   = ("127.0.0.1", 7000)
+
+		bufferSize          = 1024
+
+# Create a UDP socket at client side
+
+		UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Send to server using created UDP socket
+		if msgFromClient != "Exit":
+			UDPClientSocket.sendto(bytesToSend, serverAddressPort);
+			msgFromServer = UDPClientSocket.recvfrom(bufferSize);
+			msg = msgFromClient.format(msgFromServer[0])
+			print(msg)
+		else:
+			sys.exit("Script stopped by user")
+
+		
+		
+	
+
---
+++ UDP Receiver Broadcast/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace UDP_Receiver_Broadcast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+           
+
+
+            UDPReceiver udpReceiver = new UDPReceiver();
+            udpReceiver.StartAsync();
+        }
+    }
+}
